915639c65a11fe70c9ac390aec430afb5d8f45d2
Nguyen Thien <thiennt268@gmail.com>
2023-06-10 00:16:09 +0700

    update lan 3 CheckRoomPending

--- app/src/main/java/com/example/sunmoon/models/Conditions.java
+++ app/src/main/java/com/example/sunmoon/models/Conditions.java
@@ -17,4 +17,51 @@ public class Conditions {
         this.cDate = cDate;
         this.cAvail = cAvail;
     }
+    public String getCReportID() {
+        return cReportID;
+    }
+
+    public void setCReportID(String cReportID) {
+        this.cReportID = cReportID;
+    }
+
+    public String getCRoomNo() {
+        return cRoomNo;
+    }
+
+    public void setCRoomNo(String cRoomNo) {
+        this.cRoomNo = cRoomNo;
+    }
+
+    public String getCState() {
+        return cState;
+    }
+
+    public void setCState(String cState) {
+        this.cState = cState;
+    }
+
+    public String getCName() {
+        return cName;
+    }
+
+    public void setCName(String cName) {
+        this.cName = cName;
+    }
+
+    public String getCDate() {
+        return cDate;
+    }
+
+    public void setCDate(String cDate) {
+        this.cDate = cDate;
+    }
+
+    public String getCAvail() {
+        return cAvail;
+    }
+
+    public void setCAvail(String cAvail) {
+        this.cAvail = cAvail;
+    }
 }
--- app/src/main/java/com/example/sunmoon/screen/CheckRoomPending.java
+++ app/src/main/java/com/example/sunmoon/screen/CheckRoomPending.java
@@ -1,9 +1,13 @@
 package com.example.sunmoon.screen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,19 +15,30 @@ import android.widget.ImageButton;
 
 import com.example.sunmoon.MainActivity;
 import com.example.sunmoon.R;
+import com.example.sunmoon.adapter.ConditionAdapter;
 import com.example.sunmoon.models.Conditions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.locks.Condition;
 
 public class CheckRoomPending extends AppCompatActivity {
     private Button addButton;
     private Dialog dialog;
     private EditText roomEditText, statusEditText, byEditText;
     private DatabaseReference databaseRef;
+    private List<Conditions> conditionList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ConditionAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -55,7 +70,7 @@ public class CheckRoomPending extends AppCompatActivity {
                         String roomNo = roomEditText.getText().toString();
                         String status = statusEditText.getText().toString();
                         String by = byEditText.getText().toString();
-                        String date = new SimpleDateFormat("HH:mm dd/MM/yyyy", Locale.getDefault()).format(new Date());
+                        String date = new SimpleDateFormat("HH:mm    dd/MM/yyyy", Locale.getDefault()).format(new Date());
                         String reportId = generateReportId();
                         Conditions conditions = new Conditions(reportId, roomNo, status, by, date, "1");
                         databaseRef = FirebaseDatabase.getInstance().getReference("Conditions");
@@ -65,10 +80,74 @@ public class CheckRoomPending extends AppCompatActivity {
                 });
             }
         });
+        recyclerView = findViewById(R.id.recyclerview);
+        adapter = new ConditionAdapter(conditionList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        DatabaseReference conditionsRef = FirebaseDatabase.getInstance().getReference("Conditions");
+        Query query = conditionsRef.orderByChild("cAvail").equalTo(1);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    conditionList.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        try {
+                            Conditions condition = snapshot.getValue(Conditions.class);
+                            conditionList.add(condition);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            // Handle the exception, display an error message, or perform any necessary error handling
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle the database error
+                // Display an error message or perform any necessary error handling
+                String errorMessage = databaseError.getMessage();
+                Log.e("Firebase Database Error", errorMessage);
+            }
+        });
+
     }
+
+    /*private void showRecyclerView() {
+        List<Conditions> conditionList = new ArrayList<>();
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        ConditionAdapter adapter = new ConditionAdapter(conditionList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        DatabaseReference conditionsRef = FirebaseDatabase.getInstance().getReference("Conditions");
+        Query query = conditionsRef.orderByChild("cAvail").equalTo("1");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    conditionList.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Conditions condition = snapshot.getValue(Conditions.class);
+                        conditionList.add(condition);
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle any errors
+            }
+        });
+
+    }*/
+
     private String generateReportId() {
-        // aa
-        int currentReportId = 0;
+        //
+        int currentReportId = 4;
         currentReportId++;
         String formattedReportId = String.format(Locale.getDefault(), "c%03d", currentReportId);
         return formattedReportId;
