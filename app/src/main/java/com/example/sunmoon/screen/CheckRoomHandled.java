26ce1aa3c0a98a242d79287afff807b2326e9a35
Nguyen Thien <thiennt268@gmail.com>
2023-06-10 12:17:16 +0700

    update RecyclerView CheckRoomPending

--- app/src/main/java/com/example/sunmoon/screen/CheckRoomHandled.java
+++ app/src/main/java/com/example/sunmoon/screen/CheckRoomHandled.java
@@ -1,14 +1,29 @@
 package com.example.sunmoon.screen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.sunmoon.R;
 
 public class CheckRoomHandled extends AppCompatActivity {
-
+    private AppCompatButton pendingButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkroom_handled);
+        pendingButton = findViewById(R.id.btn_Pending);
+        pendingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckRoomHandled.this, CheckRoomPending.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sunmoon/screen/CheckRoomPending.java
+++ app/src/main/java/com/example/sunmoon/screen/CheckRoomPending.java
@@ -2,26 +2,26 @@ package com.example.sunmoon.screen;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-import com.example.sunmoon.MainActivity;
 import com.example.sunmoon.R;
-import com.example.sunmoon.adapter.ConditionAdapter;
+import com.example.sunmoon.adapter.RecyclerViewAdapter;
 import com.example.sunmoon.models.Conditions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
@@ -29,7 +29,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.concurrent.locks.Condition;
 
 public class CheckRoomPending extends AppCompatActivity {
     private Button addButton;
@@ -38,7 +37,8 @@ public class CheckRoomPending extends AppCompatActivity {
     private DatabaseReference databaseRef;
     private List<Conditions> conditionList = new ArrayList<>();
     private RecyclerView recyclerView;
-    private ConditionAdapter adapter;
+    private RecyclerViewAdapter adapter;
+    private AppCompatButton handledButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -72,7 +72,8 @@ public class CheckRoomPending extends AppCompatActivity {
                         String by = byEditText.getText().toString();
                         String date = new SimpleDateFormat("HH:mm    dd/MM/yyyy", Locale.getDefault()).format(new Date());
                         String reportId = generateReportId();
-                        Conditions conditions = new Conditions(reportId, roomNo, status, by, date, "1");
+                        int avail = 1;
+                        Conditions conditions = new Conditions(reportId, roomNo, status, by, date, avail);
                         databaseRef = FirebaseDatabase.getInstance().getReference("Conditions");
                         databaseRef.child(reportId).setValue(conditions);
                         dialog.dismiss();
@@ -80,74 +81,53 @@ public class CheckRoomPending extends AppCompatActivity {
                 });
             }
         });
-        recyclerView = findViewById(R.id.recyclerview);
-        adapter = new ConditionAdapter(conditionList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-        DatabaseReference conditionsRef = FirebaseDatabase.getInstance().getReference("Conditions");
-        Query query = conditionsRef.orderByChild("cAvail").equalTo(1);
-
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        handledButton = findViewById(R.id.btn_Handled);
+        handledButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    conditionList.clear();
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        try {
-                            Conditions condition = snapshot.getValue(Conditions.class);
-                            conditionList.add(condition);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            // Handle the exception, display an error message, or perform any necessary error handling
-                        }
-                    }
-                    adapter.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Handle the database error
-                // Display an error message or perform any necessary error handling
-                String errorMessage = databaseError.getMessage();
-                Log.e("Firebase Database Error", errorMessage);
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckRoomPending.this, CheckRoomHandled.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
         });
-
-    }
-
-    /*private void showRecyclerView() {
-        List<Conditions> conditionList = new ArrayList<>();
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        ConditionAdapter adapter = new ConditionAdapter(conditionList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView = findViewById(R.id.recyclerview);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerViewAdapter();
         recyclerView.setAdapter(adapter);
-        DatabaseReference conditionsRef = FirebaseDatabase.getInstance().getReference("Conditions");
-        Query query = conditionsRef.orderByChild("cAvail").equalTo("1");
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference conditionsRef = database.getReference("Conditions");
+        conditionsRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    conditionList.clear();
+                try {
+                    List<Conditions> filteredConditions = new ArrayList<>();
+
                     for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                         Conditions condition = snapshot.getValue(Conditions.class);
-                        conditionList.add(condition);
+
+                        if (condition != null && condition.getAvail() == 1) {
+                            filteredConditions.add(condition);
+                        }
                     }
+                    adapter.setData(filteredConditions);
                     adapter.notifyDataSetChanged();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(CheckRoomPending.this, "Error occurred: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // Handle any errors
+                Toast.makeText(CheckRoomPending.this, "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-
-    }*/
-
+    }
     private String generateReportId() {
         //
-        int currentReportId = 4;
+        int currentReportId = 2;
         currentReportId++;
         String formattedReportId = String.format(Locale.getDefault(), "c%03d", currentReportId);
         return formattedReportId;
