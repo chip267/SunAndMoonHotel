8ac5e5dc8f4965a5a5ba43182a17fea39f6b3b3b
Tlyl1111 <yenlinhtranle1111@gmail.com>
2023-06-11 15:42:29 +0700

    change Home change HouseKeeping

--- app/src/main/java/com/example/sunmoon/screen/AllRoom.java
+++ app/src/main/java/com/example/sunmoon/screen/AllRoom.java
@@ -2,7 +2,10 @@ package com.example.sunmoon.screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.sunmoon.R;
 
@@ -12,6 +15,17 @@ public class AllRoom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.rooms_page);
         super.onCreate(savedInstanceState);
+
+        ImageView back = findViewById(R.id.imageView4);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
 
     }
