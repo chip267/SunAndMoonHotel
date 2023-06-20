5bdc1292ce6e4e0a04639f403e8be4fa366c9f90
Admin <tranthanhhien123bt@gmail.com>
2023-06-21 02:04:27 +0700

    update sort Vacant

--- app/src/main/java/com/example/sunmoon/screen/Vacant.java
+++ app/src/main/java/com/example/sunmoon/screen/Vacant.java
@@ -3,14 +3,17 @@ package com.example.sunmoon.screen;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.sunmoon.R;
@@ -18,6 +21,8 @@ import com.example.sunmoon.adapter.BookedRoomAdapter;
 import com.example.sunmoon.adapter.GuestAdapter;
 import com.example.sunmoon.adapter.RecyclerViewAdapter;
 import com.example.sunmoon.adapter.RecyclerViewHandledAdapter;
+import com.example.sunmoon.adapter.RoomSortAdapter;
+import com.example.sunmoon.adapter.RoomTypeAdapter;
 import com.example.sunmoon.adapter.VacantAdapter;
 import com.example.sunmoon.models.Booking;
 import com.example.sunmoon.models.Conditions;
@@ -32,6 +37,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Vacant extends AppCompatActivity implements VacantAdapter.OnButtonClickListener {
@@ -42,6 +50,13 @@ public class Vacant extends AppCompatActivity implements VacantAdapter.OnButtonC
     ImageView btn_back;
 
     Button allroom, booked;
+
+    Spinner filter, sort;
+    RoomTypeAdapter roomTypeAdapter;
+    RoomSortAdapter roomSortAdapter;
+    ArrayList <String> roomTypeList;
+    SearchView searchRoom;
+    private String textsrc="", filterOption, sortOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,6 +114,7 @@ public class Vacant extends AppCompatActivity implements VacantAdapter.OnButtonC
                             filteredRoom.add(room);
                         }
                     }
+                    listroom = filteredRoom;
                     adapter.setData(filteredRoom);
                     adapter.notifyDataSetChanged();
                 } catch (Exception e) {
@@ -113,6 +129,34 @@ public class Vacant extends AppCompatActivity implements VacantAdapter.OnButtonC
             }
         });
 
+        searchRoom = findViewById(R.id.search_vacant);
+        searchRoom.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                textsrc = newText;
+                filterOption = filter.getSelectedItem().toString();
+                sortOption = sort.getSelectedItem().toString();
+                filteredList(newText, filterOption, sortOption);
+                return false;
+            }
+        });
+
+        filter = findViewById(R.id.sp_Filter);
+        roomTypeList = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.roomtype)));
+        roomTypeAdapter = new RoomTypeAdapter(this, roomTypeList);
+        filter.setAdapter(roomTypeAdapter);
+        filter.setOnItemSelectedListener(new RoomTypeSpinnerClass());
+
+        sort = findViewById(R.id.sp_Sort);
+        roomSortAdapter = new RoomSortAdapter(this, new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.roomsort))));
+        sort.setAdapter(roomSortAdapter);
+        sort.setOnItemSelectedListener(new RoomSortSpinnerClass());
+
 
     }
     public void onButtonClick(String roomID) {
@@ -121,4 +165,81 @@ public class Vacant extends AppCompatActivity implements VacantAdapter.OnButtonC
         startActivity(intent);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
+    private void filteredList (String searchText, String option, String _sortOption){
+        List<Room> filterList  = new ArrayList<>();
+
+        for (Room item : listroom){
+            if (item.getRoomID().toLowerCase().contains(searchText.toLowerCase())){
+                if (option.equalsIgnoreCase("All")){
+                    filterList.add(item);
+                }
+                else{
+                    if (option.equalsIgnoreCase(item.getRoomType())){
+                        filterList.add(item);
+                    }
+                }
+            }
+        }
+        if (searchText.isEmpty()){
+            if (option.equalsIgnoreCase("All")){
+                filterList = listroom;
+            }
+            else {
+                filterList.clear();
+                for (Room item : listroom){
+                    if (item.getRoomID().toLowerCase().contains(searchText.toLowerCase())){
+                        if (option.equalsIgnoreCase(item.getRoomType())){
+                            filterList.add(item);
+                        }
+                    }
+                }
+            }
+
+            //searchRoom.clearFocus();
+        }
+        else if (filterList.isEmpty() && !searchText.isEmpty()){
+            filterList.clear();
+        }
+        if (_sortOption.equalsIgnoreCase("Ascending price")){
+            Collections.sort(filterList, Room.AscendingPriceComparator);
+        }
+        else if (_sortOption.equalsIgnoreCase("Descending price")){
+            Collections.sort(filterList, Room.DescendingPriceComparator);
+        }
+
+        adapter.setFilteredItem(filterList);
+    }
+
+    class RoomTypeSpinnerClass implements AdapterView.OnItemSelectedListener
+    {
+        public void onItemSelected(AdapterView<?> parent, View v, int position, long id)
+        {
+            filterOption = filter.getSelectedItem().toString();
+            sortOption = sort.getSelectedItem().toString();
+            searchRoom.clearFocus();
+            filteredList(textsrc, filterOption, sortOption);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    }
+
+    class RoomSortSpinnerClass implements AdapterView.OnItemSelectedListener
+    {
+        public void onItemSelected(AdapterView<?> parent, View v, int position, long id)
+        {
+            filterOption = filter.getSelectedItem().toString();
+            sortOption = sort.getSelectedItem().toString();
+            searchRoom.clearFocus();
+            filteredList(textsrc, filterOption, sortOption);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    }
+
 }
