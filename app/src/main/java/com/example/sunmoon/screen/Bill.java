a6fea113cd66980409b1dffb3d2a0c602ba41310
Tlyl1111 <yenlinhtranle1111@gmail.com>
2023-07-09 01:05:36 +0700

    Add ExportBill

--- app/src/main/java/com/example/sunmoon/screen/Bill.java
+++ app/src/main/java/com/example/sunmoon/screen/Bill.java
@@ -3,17 +3,30 @@ package com.example.sunmoon.screen;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.pdf.PdfDocument;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,10 +40,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 public class Bill extends AppCompatActivity implements Serializable {
     private TextView tvCheckin, tvCheckout, tvRoomNum, tvType, tvRoomCharge, tvDetails, tvSurcharge, tvTotal, tvBookID, tvNameCus, tvPhoneCus;
+    private Button btn_export;
+    final  static  int REQUEST_CODE = 1232;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,5 +98,116 @@ public class Bill extends AppCompatActivity implements Serializable {
         tvBookID.setText(bbookID);
         tvNameCus.setText(bnameCus);
         tvPhoneCus.setText(bphoneCus);
+        askPermission();
+        btn_export = findViewById(R.id.btn_exportBill);
+        btn_export.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkWriteExternalPermission())
+                     convertXMLtoPDF();
+                else askPermission();
+            }
+        });
+    }
+
+    private boolean checkWriteExternalPermission()
+    {
+        String permission = android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        int res = getApplicationContext().checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+    @SuppressLint("MissingInflatedId")
+    private void convertXMLtoPDF() {
+        View view = LayoutInflater.from(this).inflate(R.layout.booking_bill,null);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+
+       view.findViewById(R.id.layout_visible).setVisibility(View.INVISIBLE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            this.getDisplay().getRealMetrics(displayMetrics);
+
+        }else this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        view.measure(View.MeasureSpec.makeMeasureSpec(displayMetrics.widthPixels, View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.makeMeasureSpec(displayMetrics.heightPixels, View.MeasureSpec.EXACTLY));
+
+        view.layout(0,0,displayMetrics.widthPixels, displayMetrics.heightPixels);
+        PdfDocument document = new PdfDocument();
+
+        int viewWidth = view.getMeasuredWidth();
+        int viewHeight = view.getMeasuredHeight();
+        Log.d("my log", "Width: "+viewWidth);
+
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(1080, 1920, 1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+        view.draw(canvas);
+
+        document.finishPage(page);
+
+        File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        String fileName = tvBookID.getText() + ".pdf";
+        File file = new File(downloadsDir, fileName);
+        try{
+            FileOutputStream fos = new FileOutputStream(file);
+            document.writeTo(fos);
+            document.close();
+            fos.close();
+            Toast.makeText(this, "Convert to XML Sucessfully!!!", Toast.LENGTH_SHORT).show();
+
+        }catch (FileNotFoundException e){
+            Log.d("my log", "Error while writting"+ e.toString());
+            throw new RuntimeException(e);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        view.findViewById(R.id.layout_visible).setVisibility(View.VISIBLE);
+    }
+
+    private void askPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+
+    }
+
+    private  void createPDF(){
+
+        PdfDocument document = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(1080,1920,1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setTextSize(42);
+
+        String text = "Hello, World";
+        float x = 500;
+        float y = 900;
+
+        canvas.drawText(text, x,y, paint);
+        document.finishPage(page);
+
+        File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        String fileName = "example.pdf";
+        File file = new File(downloadsDir, fileName);
+        try{
+            FileOutputStream fos = new FileOutputStream(file);
+            document.writeTo(fos);
+            document.close();
+            fos.close();
+            Toast.makeText(this, "Written Sucessfully!!!", Toast.LENGTH_SHORT).show();
+
+        }catch (FileNotFoundException e){
+            Log.d("my log", "Error while writting"+ e.toString());
+            throw new RuntimeException(e);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+        convertXMLtoPDF();
+
     }
 }
