bb53a1bd7a4fb133113cdeadeb086fcc0b649625
Tlyl1111 <yenlinhtranle1111@gmail.com>
2023-07-12 18:33:15 +0700

    Change ExportBill

--- app/src/main/java/com/example/sunmoon/screen/Bill.java
+++ app/src/main/java/com/example/sunmoon/screen/Bill.java
@@ -104,7 +104,7 @@ public class Bill extends AppCompatActivity implements Serializable {
             @Override
             public void onClick(View v) {
                 if (checkWriteExternalPermission())
-                     convertXMLtoPDF();
+                     createPDF();
                 else askPermission();
             }
         });
@@ -115,13 +115,14 @@ public class Bill extends AppCompatActivity implements Serializable {
         String permission = android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
         int res = getApplicationContext().checkCallingOrSelfPermission(permission);
         return (res == PackageManager.PERMISSION_GRANTED);
-    }
+    }/*
     @SuppressLint("MissingInflatedId")
     private void convertXMLtoPDF() {
-        View view = LayoutInflater.from(this).inflate(R.layout.booking_bill,null);
+        View view = LayoutInflater.from(this).inflate(R.layout.booking_bill_large,null);
         DisplayMetrics displayMetrics = new DisplayMetrics();
 
-       view.findViewById(R.id.layout_visible).setVisibility(View.INVISIBLE);
+       view.findViewById(R.id.layout_header).setVisibility(View.INVISIBLE);
+       view.findViewById(R.id.btn_exportBill).setVisibility(View.INVISIBLE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
             this.getDisplay().getRealMetrics(displayMetrics);
@@ -164,13 +165,21 @@ public class Bill extends AppCompatActivity implements Serializable {
             throw new RuntimeException(e);
         }
 
-        view.findViewById(R.id.layout_visible).setVisibility(View.VISIBLE);
-    }
+        view.findViewById(R.id.layout_header).setVisibility(View.VISIBLE);
+        view.findViewById(R.id.btn_exportBill).setVisibility(View.VISIBLE);
+    }*/
 
     private void askPermission() {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
 
     }
+    private float returnX (Paint paint,Canvas canvas, String text)
+    {
+        float textWidth = paint.measureText(text);
+        float x = (canvas.getWidth() - textWidth) + 130 ;
+        return x;
+    }
+
 
     private  void createPDF(){
 
@@ -181,33 +190,111 @@ public class Bill extends AppCompatActivity implements Serializable {
         Canvas canvas = page.getCanvas();
 
         Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setTextSize(42);
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(60);
+        paint.setStrokeWidth(8);
 
-        String text = "Hello, World";
-        float x = 500;
-        float y = 900;
+        Paint paintbold = new Paint();
+        paintbold.setColor(Color.BLACK);
+        paintbold.setTextSize(45);
+        paintbold.setStrokeWidth(8);
 
-        canvas.drawText(text, x,y, paint);
+        Paint paintbody = new Paint();
+        paintbody.setColor(Color.BLACK);
+        paintbody.setTextSize(35);
+
+        String text = "Sun&Moon Bill";
+        String id = "ID";
+        String tab ="_______________________________";
+        String tID = tvBookID.getText().toString();
+        String checkin = "Check in" ;
+        String timeCI = tvCheckin.getText().toString();
+        String checkout = "Check out";
+        String timeCO = tvCheckout.getText().toString();
+        String room = "Room";
+        String tRoom = tvRoomNum.getText().toString();
+        String bookingtype = "Type of Booking";
+        String tTypeB = tvType.getText().toString();
+
+        String roomcharge = "Room Charge";
+        String tRoomC = tvRoomCharge.getText().toString();
+        String tDetail = tvDetails.getText().toString();
+        String surcharge = "Surcharge";
+        String tSurC = tvSurcharge.getText().toString();
+
+        String total = "Total";
+        String tTotal = tvTotal.getText().toString();
+
+        String phonNo = "Phone No.";
+        String tPhoneNo = "+84 (0) 749829256";
+        String email = "Email";
+        String tEmail = "sunmoon@gmail.com";
+
+        String cus = "Customer";
+        String tCus = tvNameCus.getText().toString();
+        String phonecus = "Phone No.";
+        String tPhoneCus = tvPhoneCus.getText().toString();
+
+
+        canvas.drawText(text, (canvas.getWidth() - paint.measureText(text))/2+10,250, paint);
+        canvas.drawText(tab, (canvas.getWidth() - paint.measureText(text))/2 -40 ,300, paintbody);
+
+        canvas.drawText(id, 150,400, paintbody);
+        canvas.drawText(tID,returnX(paint,canvas,timeCI),400,paintbody);
+
+        canvas.drawText(checkin, 150,470, paintbody);
+        canvas.drawText(timeCI,returnX(paint,canvas,timeCI),470,paintbody);
+
+        canvas.drawText(checkout, 150,540, paintbody);
+        canvas.drawText(timeCO,returnX(paint,canvas,timeCO),540,paintbody);
+
+        canvas.drawText(room, 150,610, paintbody);
+        canvas.drawText(tRoom,returnX(paint,canvas,timeCO),610,paintbody);
+
+        canvas.drawText(bookingtype, 150,680, paintbody);
+        canvas.drawText(tTypeB,returnX(paint,canvas,timeCO),680,paintbody);
+
+        canvas.drawText(roomcharge, 150,830, paintbold);
+        canvas.drawText(tRoomC,returnX(paint,canvas,timeCO)+20,830,paintbold);
+        canvas.drawText(tDetail, 150,900, paintbody);
+        canvas.drawText(surcharge, 150,970, paintbold);
+        canvas.drawText(tSurC,returnX(paint,canvas,timeCO)+20,970,paintbold);
+
+        canvas.drawText(tab, (canvas.getWidth() - paint.measureText(text))/2 -40 ,1030, paintbody);
+        canvas.drawText(total, 150,1130, paint);
+        canvas.drawText(tTotal,returnX(paint,canvas,timeCO)+20,1130,paint);
+
+        canvas.drawText(phonNo, 150,1280, paintbody);
+        canvas.drawText(tPhoneNo,returnX(paint,canvas,timeCO),1280,paintbody);
+
+        canvas.drawText(email, 150,1350, paintbody);
+        canvas.drawText(tEmail,returnX(paint,canvas,timeCO),1350,paintbody);
+        canvas.drawText(tab, (canvas.getWidth() - paint.measureText(text))/2 -40 ,1400, paintbody);
+
+        canvas.drawText(cus, 150,1460, paintbody);
+        canvas.drawText(tCus,returnX(paint,canvas,timeCO),1460,paintbody);
+        canvas.drawText(phonecus, 150,1530, paintbody);
+        canvas.drawText(tPhoneCus,returnX(paint,canvas,timeCO),1530,paintbody);
+
         document.finishPage(page);
 
         File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        String fileName = "example.pdf";
+        String fileName = tvBookID.getText() + ".pdf";
         File file = new File(downloadsDir, fileName);
         try{
             FileOutputStream fos = new FileOutputStream(file);
             document.writeTo(fos);
             document.close();
             fos.close();
-            Toast.makeText(this, "Written Sucessfully!!!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Export Bill Sucessfully!!!", Toast.LENGTH_SHORT).show();
 
         }catch (FileNotFoundException e){
-            Log.d("my log", "Error while writting"+ e.toString());
+            Log.d("my log", "Error while exporting"+ e.toString());
             throw new RuntimeException(e);
         }catch (IOException e){
             throw new RuntimeException(e);
         }
-        convertXMLtoPDF();
+
 
     }
 }
